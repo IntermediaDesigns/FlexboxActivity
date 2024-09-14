6a5cf26fda1f0b9925f07fa29e233e3d1ad8e7a6
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-14 22:58:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,146 @@
+    document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch1_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch1_flex").value;
+
+    const css = `
+      .ch1_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
+
+    document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch2_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch2_flex").value;
+
+    const css = `
+      .ch2_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch3_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch3_flex").value;
+
+    const css = `
+      .ch3_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch4_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch4_flex").value;
+
+    const css = `
+      .ch4_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
+
+    document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch5_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch5_flex").value;
+
+    const css = `
+      .ch5_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
+
+    document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch6_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch6_flex").value;
+
+    const css = `
+      .ch6_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
+
+   document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch7_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch7_flex").value;
+
+    const css = `
+      .ch7_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+      const cssForm = document.getElementById("ch8_cssForm");
+      const customStyle = document.getElementById("custom-style");
+
+      cssForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const flex = document.getElementById("ch8_flex").value;
+
+    const css = `
+      .ch8_coins {${flex};
+}
+    `;
+
+    customStyle.innerHTML = css;
+  });
+});
